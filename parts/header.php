b256d626b65c0f7e02ffd80c4867c66810b1bd56
Kritponn <martinrizling@gmail.com>
2024-04-24 14:15:54 +0200

    adding-admin

--- parts/header.php
+++ parts/header.php
@@ -9,7 +9,7 @@
         elseif ($stranka == 'about.php' ) { ?> Niečo málo o nás <?php } ?>
     </h2>
     <p class="col-12 text-center">
-    <?php  if ($stranka == 'index.php' ) {?> Vitajte na naších stránkach Simple House. Reštaurácia Simple House je spojením škandinávskeho dizajnu a kulinárskeho umenia. Príjemná atmosféra a prostredie predurčujú k tomu, ako si najlepšie vychutnať skvelé jedlo s vašimi rodinami či obchodnými partnermi. <?php }
+    <?php  if ($stranka == 'index.php' ) {?> Vitajte na naších stránkach Reštaurička. Reštaurácia "Reštaurička" je spojením škandinávskeho dizajnu a kulinárskeho umenia. Príjemná atmosféra a prostredie predurčujú k tomu, ako si najlepšie vychutnať skvelé jedlo s vašimi rodinami či obchodnými partnermi. <?php }
     elseif ($stranka == 'contact.php' ) { ?> Sme tu pre vás. Preto nás neváhajte osloviť s akoukoľvek požiadavkou alebo otázkou. Radi vás našou reštauráciou prevedieme, zoznámime vás s našim menu, poradíme čo si vybrať.  <?php }
     elseif ($stranka == 'about.php' ) { ?> Naša reštaurácia je kolektív úžasných ľudí, sme profesionáli, ktorí milujú svoje remeslo. <br>
         Láska k remeslu a odborná zručnosť sú vstupným požiadavkom na každého nášho zamestnanca. A okrem toho samorejme zdvorilý prístup a otvorenosť.<?php } ?>

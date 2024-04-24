57b92e04e8982223957e1603846fe771f68f93e9
Kritponn <martinrizling@gmail.com>
2024-04-24 13:35:55 +0200

    uprava-header

--- parts/footer.php
+++ parts/footer.php
@@ -1,5 +1,5 @@
 <footer class="tm-footer text-center">
-    <p>Copyright &copy; 2024 Simple House
+    <p>Copyright &copy; 2024 Reštaurička
 
         | Design: <a rel="nofollow" href="https://templatemo.com">TemplateMo</a></p>
     

671c90362dad9a13a6e02d39e5b41162af41b2e7
Kriptonnn <martinrizling@gmail.com>
2024-05-29 12:45:48 +0200

    br

--- parts/footer.php
+++ parts/footer.php
@@ -1,5 +1,9 @@
 <footer class="tm-footer text-center">
     <p>Copyright &copy; 2020 Simple House
+        <br>
+        <br>
+        <br>
+        <br>
 
         | Design: <a rel="nofollow" href="https://templatemo.com">TemplateMo</a></p>
     <a href="admin/login.php"> Admin </a>
